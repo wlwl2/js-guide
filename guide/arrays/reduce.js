95047ff54e4c629766584d0e34aa0e3bd9f0c9be
William Lim <manvinedi@gmail.com>
2019-08-12 19:32:17 +0800

    updated reducer

--- guide/arrays/reduce.js
+++ guide/arrays/reduce.js
@@ -1,9 +1,3 @@
-# Array.prototype.reduce()
-
-Executes a reducer function on each element of the array,
-resulting in a single output value.
-
-```js
 const numberGroupToBeReduced = [2, 4, 6, 8]
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue
@@ -11,4 +5,3 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue
 const numberGroupReduced = numberGroupToBeReduced.reduce(reducer)
 
 console.log(numberGroupReduced) // logs: 20
-```
